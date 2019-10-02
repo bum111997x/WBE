e9c9e5062e2b51a1e11240b2ea584067326dd684
bum111997x <bum111997x@gmail.com>
2019-10-02 16:52:22 +0700

    [Bài tập] Hiển thị thông báo nếu người dùng nhập số bất hợp lệ

--- ss2/displayNumber.php
+++ ss2/displayNumber.php
@@ -14,20 +14,16 @@ function calc($x, $y)
     echo "<br>";
 
     try {
-        condition($x, $y);
+        if ($y == 0) {
+            throw new Exception(" Xảy ra ngoại lệ ");
+        } else {
+            echo "$x / $y = " . $x / $y;
+        }
     } catch (Exception $e) {
         echo 'Mess: ' . $e->getMessage(), "\n";
     }
 }
 
-calc(9,1);
-function condition($x, $y)
-{
-    if ($y == 0) {
-        throw new Exception(" Xảy ra ngoại lệ ");
-    } else {
-        echo "$x / $y = " . $x / $y;
-    }
-}
+calc(9,0);
 
 ?>
