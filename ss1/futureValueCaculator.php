3d7ef463830f70c197495d196340017a91cb8c4f
bum111997x <bum111997x@gmail.com>
2019-10-02 11:09:32 +0700

    [Bài tập] Ứng dụng Future Value Calculator

--- ss1/futureValueCaculator.php
+++ ss1/futureValueCaculator.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] = "POST"){
     $yearlyInterestRate = $_POST["yearlyInterestRate"];
     $numberOfYears = $_POST["numberOfYears"];
     $futerValue = $investmentAmount + ($investmentAmount * $yearlyInterestRate /100)*$numberOfYears;
-    echo "Investment Amount: $investmentAmount";
+    echo "Investment Amount:  $investmentAmount";
     echo "<br>";
     echo "Yearly Interest Rate: $yearlyInterestRate%";
     echo "<br>";
