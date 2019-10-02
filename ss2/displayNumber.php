6599d1f6ce8520e177588e16f72dcde9d5cf36fd
bum111997x <bum111997x@gmail.com>
2019-10-02 16:49:20 +0700

    [Bài tập] Hiển thị thông báo nếu người dùng nhập số bất hợp lệ

--- ss2/displayNumber.php
+++ ss2/displayNumber.php
@@ -5,27 +5,29 @@ function calc($x, $y)
     $tong = $x + $y;
     $hieu = $x - $y;
     $nhan = $x * $y;
-    $chia = $x / $y;
 
     echo "$x + $y = $tong";
-
     echo "<br>";
     echo "$x - $y = $hieu";
     echo "<br>";
     echo "$x * $y = $nhan";
     echo "<br>";
-    echo "$x / $y = $chia";
-    echo "<br>";
-    if ($y == 0) {
-        throw new Exception(" Xảy ra ngoại lệ ");
+
+    try {
+        condition($x, $y);
+    } catch (Exception $e) {
+        echo 'Mess: ' . $e->getMessage(), "\n";
     }
-//    return true;
 }
 
-try {
-    echo calc(0, 0) . "\n";
-} catch (Exception $e) {
-    echo 'Mess: ' . $e->getMessage(), "\n";
+calc(9,1);
+function condition($x, $y)
+{
+    if ($y == 0) {
+        throw new Exception(" Xảy ra ngoại lệ ");
+    } else {
+        echo "$x / $y = " . $x / $y;
+    }
 }
 
 ?>
