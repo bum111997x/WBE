7d86f1c67ffa8569b970939d2c15a6d6c980f20f
bum111997x <bum111997x@gmail.com>
2019-10-02 11:15:55 +0700

    [Thực hành] Ứng dụng hiển thị thời gian của server

--- ss1/showtime.php
+++ ss1/showtime.php
@@ -11,7 +11,7 @@ echo "Xin Chao!"
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Thoi gian hien tai</title>
+    <title>Thoi gian hien tai </title>
 </head>
 <body>
 <h1>Bay gio la: <?php echo date('Y-M-d h:m:s'); ?></h1>
